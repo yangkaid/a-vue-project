15acbb2352a39ce5e69616e31f4e66cddd9484da
yangkaid <1376086260@qq.com>
2023-01-28 12:51:57 +0800

    响应式盒子的CSS

--- src/views/TestView/index.vue
+++ src/views/TestView/index.vue
@@ -1,6 +1,25 @@
 <template>
   <div class='test-view'>我是测试页面</div>
+  <div class="rect-content">
+    <div class="rect">响应式1</div>
+    <div class="rect">响应式2</div>
+    <div class="rect">响应式3</div>
+    <div class="rect">响应式4</div>
+  </div>
 </template>
 <script setup></script>
 <style lang='scss' scoped>
+.rect-content {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+}
+.rect {
+  flex-shrink: 1;
+  min-width: 300px;
+  height: 200px;
+  background-color: var(--el-color-primary);
+  padding: 20px;
+  font-size: 14px;
+}
 </style>
